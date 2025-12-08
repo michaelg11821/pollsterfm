35f73ea8b1c939c0ec0c5f3b6965662354adc40f
leavesturninsideyou <134650278+leavesturninsideyou@users.noreply.github.com>
2025-12-08 00:25:42 -0800

    add img storage id queries

--- src/lib/convex/files.ts
+++ src/lib/convex/files.ts
@@ -1,9 +1,44 @@
+import { getAuthUserId } from "@convex-dev/auth/server";
 import { v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const generateUploadUrl = mutation({
   handler: async (ctx) => {
     return await ctx.storage.generateUploadUrl();
+  },
+});
+
+export const getProfileIconStorageId = query({
+  handler: async (ctx) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (userId === null) return null;
+
+    const userImageIds = await ctx.db
+      .query("userImageIds")
+      .withIndex("by_userId", (q) => q.eq("userId", userId))
+      .unique();
+
+    if (userImageIds === null) return null;
+
+    return userImageIds.profileIconId;
+  },
+});
+
+export const getHeaderImageStorageId = query({
+  handler: async (ctx) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (userId === null) return null;
+
+    const userImageIds = await ctx.db
+      .query("userImageIds")
+      .withIndex("by_userId", (q) => q.eq("userId", userId))
+      .unique();
+
+    if (userImageIds === null) return null;
+
+    return userImageIds.headerImageId;
   },
 });
 
